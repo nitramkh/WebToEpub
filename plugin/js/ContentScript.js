5074302c685b809ddfa8cac4a78274ddea1b55a1
nitramkh <nitramkh@gmail.com>
2026-03-28 02:59:38 +0100

    [JSDOC][ContentScript.js] Amalgamated commit

--- plugin/js/ContentScript.js
+++ plugin/js/ContentScript.js
@@ -1,7 +1,7 @@
 /*
-  Javascript that is injected into active tab.
-  Returns the DOM of the window's contents
-*/
+ * Javascript that is injected into active tab.
+ * Returns the DOM of the window's contents
+ */
 "use strict";
 
 var parseResults = { 
@@ -9,4 +9,5 @@ var parseResults = {
     document: document.all[0].outerHTML,
     url: document.URL
 };
+
 chrome.runtime.sendMessage(parseResults);
