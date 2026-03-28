afcbcb0e9398d24279f74c8a678de91e45d1cc41
nitramkh <nitramkh@gmail.com>
2026-03-28 02:02:51 +0100

    [JSDOC][ErrorLog.js] Amalgamated commit

--- plugin/js/ErrorLog.js
+++ plugin/js/ErrorLog.js
@@ -1,21 +1,48 @@
 "use strict";
 
 class ErrorLog {
-    constructor() {
-    }
+    /**
+     * Whether to prevent any errors from being shown to the user. Note that
+     * this is ignored if the error has a retry action.
+     * 
+     * @type { boolean }
+     * @public
+     */
     static SuppressErrorLog =  false;
 
+    /**
+     * @private
+     */
+    constructor() {}
+
+    /**
+     * Log an error but don't display it to the user in the UI.
+     * 
+     * @param { string | Error | CustomErrorMessage } error The error.
+     * @returns { void } Changes are made on state directly.
+     * 
+     * @public
+     */
     static log(error) {
         ErrorLog.history.push(ErrorLog.getMsgText(error));
     }
 
+    /**
+     * Show an error message in the UI, if one is already showing add the new
+     * one to the queue to be shown after.
+     * 
+     * @param { string | Error | CustomErrorMessage } msg The error to show.
+     * @returns { void } Changes are made on UI and state directly.
+     * 
+     * @public
+     */
     static showErrorMessage(msg) {
-        // if already showing an error message, queue the new one to display
-        // when currently showing is closed. 
         if (this.SuppressErrorLog && msg.retryAction == null) {
             return;
         }
+
         ErrorLog.queue.push(msg);
+
         if (1 < ErrorLog.queue.length) {
             return;
         }
@@ -27,8 +54,18 @@ class ErrorLog {
         ErrorLog.setErrorMessageButtons(msg, sections);
     }
 
+    /**
+     * Handler for when a "UI-error" is closed, either shows next error in queue
+     * or restores UI to it's pre-error visibility state.
+     * 
+     * @param { Map<HTMLElement, boolean> } sections The sections whose visibility is to be restored after errors are resolved.
+     * @returns { void } Changes are made on UI/state directly.
+     * 
+     * @private
+     */
     static onCloseError(sections) {
         ErrorLog.queue.shift();
+
         if (ErrorLog.queue.length === 0) {
             ErrorLog.restoreSectionVisibility(sections);
         } else {
@@ -37,87 +74,158 @@ class ErrorLog {
         }
     }
 
+    /**
+     * Show entire error log in UI.
+     * 
+     * @returns { void } Changes are made on UI directly.
+     * 
+     * @public
+     */
     static showLogToUser() {
         let history = ErrorLog.dumpHistory();
+
         if (!util.isNullOrEmpty(history)) {
             ErrorLog.showErrorMessage(history);
         }
     }
 
+    /**
+     * Clear the entire error history.
+     * 
+     * @returns { void } Changes are made on state directly.
+     * 
+     * @public
+     */
     static clearHistory() {
         ErrorLog.history = [];
     }
 
+    /**
+     * Get the entire error history as a newline separated string.
+     * 
+     * @returns { string } The error history.
+     * 
+     * @public
+     */
     static dumpHistory() {
         let errors = ErrorLog.history.join("\r\n\r\n");
+
         return errors;
     }
 
-    /** private */
+    /**
+     * Get reference to the error section in the UI.
+     * 
+     * @returns { HTMLElement | null } 
+     * 
+     * @private
+     */
     static getErrorSection() {
         return document.getElementById("errorSection");
     }
 
-    /** private */
+    /**
+     * Get all <section> tags in UI as keys and hide them, but remember previous
+     * visibility state as value.
+     * 
+     * @returns { Map<HTMLElement, boolean> } Literally all the section elements in the UI, and their "pre-error" visibility state.
+     * 
+     * @private
+     */
     static hideAllSectionsSavingVisibility() {
+        /** @type { Map<HTMLElement, boolean> } */
         let sections = new Map();
+
         for (let section of document.querySelectorAll("section")) {
             sections.set(section, section.hidden);
             section.hidden = true;
         }
+
         return sections;
     }
 
-    /** private */
+    /**
+     * Show an error message in the UI.
+     * 
+     * @param { string | Error | CustomErrorMessage } msg The error to show message for.
+     * @returns { void } Changes are made on UI directly.
+     * 
+     * @private 
+     */
     static setErrorMessageText(msg) {
-        let textRow = document.querySelector("#errorMessageText pre");
+        let textRow = /** @type { HTMLPreElement | null } */ (document.querySelector("#errorMessageText pre"));
         textRow.textContent = ErrorLog.getMsgText(msg);
     }
 
+    /**
+     * Get the error text to display in UI for an error.
+     * 
+     * @param { string | Error | CustomErrorMessage } msg The error to get text for.
+     * @returns { string } The error message text.
+     * 
+     * @private
+     */
     static getMsgText(msg) {
         if (typeof (msg) === "string") {
             return msg;
         } else {
             // assume msg is some sort of error object
             let retVal = "" + msg.stack;
+
             if (!retVal.includes(msg.message)) {
                 retVal = msg.message + " " + retVal;
             }
+
             return retVal;
         }
     }
 
-    /** private */
+    /**
+     * Set the state of error message buttons according to `msg`.
+     * 
+     * @param { string | Error | CustomErrorMessage } msg The error/settings to use.
+     * @param { Map<HTMLElement, boolean> } sections The sections whose visibility is to be restored after errors are resolved.
+     * @returns { void } Changes are made on UI directly.
+     * @private 
+     */
     static setErrorMessageButtons(msg, sections) {
         let close = () => ErrorLog.onCloseError(sections);
+
         let okButton = document.getElementById("errorButtonOk");
         let retryButton = document.getElementById("errorButtonRetry");
         let cancelButton = document.getElementById("errorButtonCancel");
         let OpenURLButton = document.getElementById("errorButtonOpenURL");
         let BlockURLButton = document.getElementById("errorButtonBlockURL");
+
         if (msg.retryAction !== undefined) {
             okButton.hidden = true;
+
             retryButton.hidden = false;
             retryButton.onclick = function() {
                 close();
                 msg.retryAction();
             };
+
             cancelButton.hidden = false;
             cancelButton.onclick = function() {
                 close();
                 msg.cancelAction();
             };
             cancelButton.textContent = UIText.Common.cancel;
+
             if (msg.cancelLabel !== undefined) {
                 cancelButton.textContent =  msg.cancelLabel;
             }
+
             if (msg.openurl !== undefined) {
                 OpenURLButton.hidden = false;
+
                 OpenURLButton.onclick = function() {
                     //window.open(new URL(msg.openurl), "_blank").focus();
                     //use chrome.tabs.create to prevent auto popup block from browser
                     chrome.tabs.create({ url: msg.openurl});
                 };
+
                 BlockURLButton.hidden = false;
                 BlockURLButton.onclick = function() {
                     close();
@@ -138,7 +246,14 @@ class ErrorLog {
         }
     }
 
-    /** private */
+    /**
+     * Set the visibility state of all key elements to value.
+     * 
+     * @param { Map<HTMLElement, boolean> } sections The sections to fix visibility for.
+     * @returns { void } Changes are made on UI directly.
+     * 
+     * @private
+     */
     static restoreSectionVisibility(sections) {
         for (let [key,value] of sections) {
             key.hidden = value;
@@ -146,5 +261,18 @@ class ErrorLog {
     }
 }
 
+/**
+ * Errors which have yet to be displayed to the user.
+ * 
+ * @type { (string | Error | CustomErrorMessage)[] }
+ */
 ErrorLog.queue = [];
+
+/**
+ * Record of past errors.
+ * 
+ * FIXME: Can't this be declared inside the class?
+ * 
+ * @type { string[] }
+ */
 ErrorLog.history = [];
