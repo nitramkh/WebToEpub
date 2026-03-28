be0e79b6da9fdedc063c7890a3d43e2401e5842c
nitramkh <nitramkh@gmail.com>
2026-03-28 03:13:02 +0100

    [JSDOC][DebugUtil.js] Amalgamated commit

--- plugin/js/DebugUtil.js
+++ plugin/js/DebugUtil.js
@@ -2,16 +2,37 @@
 "use strict";
 
 
-/** Functions to help debugging.  Not included in release product */
+/**
+ * Functions to help debugging.  Not included in release product
+ */
 class DebugUtil { // eslint-disable-line no-unused-vars
-    constructor() {
-    }
+    /**
+     * @private
+     */
+    constructor() {}
 
+    /**
+     * Convert a byte to hex.
+     * 
+     * @param { number } e The byte.
+     * @returns { string } The byte as hex.
+     * 
+     * @public
+     */
     static byteToHex(e) {
         let temp = "0" + e.toString(16);
+
         return temp.substring(temp.length - 2);
     }
 
+    /**
+     * Convert an array/buffer into a hex string.
+     * 
+     * @param { ArrayBuffer | ArrayLike<number> } buf The buffer to convert.
+     * @returns { string } The buffer as a hex string.
+     * 
+     * @public
+     */
     static bufToHex(buf) {
         return new Uint8Array(buf)
             .reduce((p, c) => p + DebugUtil.byteToHex(c), "");
