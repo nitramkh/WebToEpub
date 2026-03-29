a35f933b624683ecd017e974c4f755d7c280da7c
nitramkh <nitramkh@gmail.com>
2026-03-29 01:32:18 +0100

    [JSDOC][FakeParser.js] Amalgamated commit

--- plugin/js/debugging/FakeParser.js
+++ plugin/js/debugging/FakeParser.js
@@ -1,20 +1,31 @@
 "use strict";
 
-/**
-  This is a dummy parser, intended to act like a parser that reads a 
-  lot of chapters, slowly.  For testing other parts of WebToEpub
-  Note, need to open the by looking at site rtd.moe
-*/
-
 parserFactory.register("rtd.moe", () => new FakeParser());
 
+/**
+ * This is a dummy parser, intended to act like a parser that reads a lot of
+ * chapters, slowly. For testing other parts of WebToEpub. Note that to use it
+ * you must open the site "rtd.moe".
+ */
 class FakeParser extends Parser {
+    /**
+     * @public
+     */
     constructor() {
         super();
     }
 
-    async getChapterUrls( /* dom */ ) {
+    /**
+     * Function for fetching the links to **all** chapters.
+     * 
+     * @override
+     * @returns { Promise<ChapterLink[]> } The found chapters.
+     * 
+     * @protected
+     */
+    async getChapterUrls() {
         let chapters = [];
+
         for (let i = 1; i < 30; ++i) {
             chapters.push({
                 sourceUrl:  `https://rtd.moe/Chapter/${i}.html`,
@@ -22,17 +33,46 @@ class FakeParser extends Parser {
                 newArc: null
             });
         }
+
         return chapters;
     }
 
+    /**
+     * Function for extracting the chapter content from the chapter dom.
+     * 
+     * @override
+     * @param { Document } dom The dom of the chapter page.
+     * @returns { Element } The element holding the content.
+     * 
+     * @protected
+     */
     findContent(dom) {
         return Parser.findConstrutedContent(dom);
     }
 
+    /**
+     * Extract **story title**; override this if default implementation can't
+     * find it.
+     * 
+     * @override
+     * @param { Document } dom The document to find title in.
+     * @returns { string | Node | null } The found title, or null if not.
+     * 
+     * @protected
+     */
     extractTitleImpl(dom) {
         return dom.querySelector("h1");
     }
 
+    /**
+     * Fetch a single chapter by URL. Hook if need to chase hyperlinks in page
+     * to get all chapter content.
+     * 
+     * @param { UrlString } url Url to chapter to fetch.
+     * @returns { Promise<Document | undefined> } The fetched response.
+     * 
+     * @protected
+     */
     async fetchChapter(url) {
         let newDoc = Parser.makeEmptyDocForContent(url);
         this.addTitleToChapter(newDoc, url);
@@ -40,13 +80,30 @@ class FakeParser extends Parser {
         return newDoc.dom; 
     }
 
+    /**
+     * Create a title element using `newDoc.dom` and insert it into
+     * `newDoc.content`.
+     * 
+     * @param { { dom: Document, content: HTMLDivElement } } newDoc The document and container to use.
+     * @param { string } url The title content to set.
+     * @returns { void } Changes are made on `newDoc` directly.
+     * 
+     * @private
+     */
     addTitleToChapter(newDoc, url) {
         let title = newDoc.dom.createElement("h1");
         title.textContent = url;
         newDoc.content.appendChild(title);
     }
 
-    getInformationEpubItemChildNodes( /* dom */ ) {
+    /**
+     * Just dump an empty array.
+     * 
+     * @returns { Node[] } An empty array.
+     * 
+     * @protected
+     */
+    getInformationEpubItemChildNodes() {
         return [];
     }    
 }
